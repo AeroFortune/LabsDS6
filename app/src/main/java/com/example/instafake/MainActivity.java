4291cebe9220eeca02e758aa6ce8a3e983bec0f8
Diego Jas <extraniastravesiasdejojo@gmail.com>
2023-05-20 17:40:25 -0500

    initial ver

--- app/src/main/java/com/example/instafake/MainActivity.java
+++ app/src/main/java/com/example/instafake/MainActivity.java
@@ -17,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     ListView lstChats;
     InstagramLVAdapter adapterChats;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
