c31897cd2d75f76589f2290cd5da7a7e724f6feb
Diego Jas <extraniastravesiasdejojo@gmail.com>
2023-05-20 18:19:41 -0500

    fixes

--- app/src/main/java/com/example/instafake/MainActivity.java
+++ app/src/main/java/com/example/instafake/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     ListView lstChats;
     InstagramLVAdapter adapterChats;
 
-
+    List<Chats> chats = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +33,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private List<Chats> LlenarListView(){
-        List<Chats> chats = new ArrayList<>();
         chats.add(new Chats("Random #1", "bruh........", "2:30", "1"));
         chats.add(new Chats("Random #2", "hey dude ya hiciste el lab 4?", "2:30", "32"));
         return chats;
+    }
+
+    private void AgregarNuevoElementoListView(){
+        chats.add(new Chats("Test", "Prueba", "6:12 PM", "1"));
     }
 
 
@@ -51,8 +54,11 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
             case R.id.miNew:
-                Intent i = new Intent(getApplicationContext(), InstagramActivity.class);
-                startActivity(i);
+                // Se supone que debemos agregar un NUEVO elemento o IR a una pantalla nueva y agregarlo ahi?
+                // Lo que hice es ir con lo primero.
+                AgregarNuevoElementoListView();
+                adapterChats = new InstagramLVAdapter(getApplicationContext(), LlenarListView());
+                lstChats.setAdapter(adapterChats);
                 break;
         }
         return super.onOptionsItemSelected(item);
